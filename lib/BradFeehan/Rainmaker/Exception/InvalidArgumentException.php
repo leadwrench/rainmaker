1ad5f23c6033ab599ee4cc97bb3ff6ccac413120
Brad Feehan <git@bradfeehan.com>
2013-12-16 00:10:36 +1100

    Add InvalidArgumentException class

---
+++ lib/BradFeehan/Rainmaker/Exception/InvalidArgumentException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BradFeehan\Rainmaker\Exception;
+
+use BradFeehan\Rainmaker\Exception as RainmakerException;
+
+class InvalidArgumentException extends \InvalidArgumentException implements RainmakerException
+{
+}
