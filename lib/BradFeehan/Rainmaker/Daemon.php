e6f157cf5477eb176d642c05c38aeafce42e9995
Brad Feehan <git@bradfeehan.com>
2014-01-05 14:23:22 +1100

    Add Daemon class

---
+++ lib/BradFeehan/Rainmaker/Daemon.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace BradFeehan\Rainmaker;
+
+use BradFeehan\Rainmaker\Configuration;
+use BradFeehan\Rainmaker\Executer;
+use BradFeehan\Rainmaker\ExecuterInterface;
+
+/**
+ * Manages the running Rainmaker instance
+ */
+class Daemon
+{
+
+    /**
+     * The Configuration for this Daemon
+     *
+     * @var BradFeehan\Rainmaker\Configuration
+     */
+    private $configuration;
+
+    /**
+     * The Executer instance that this Daemon uses to run
+     *
+     * @var BradFeehan\Rainmaker\ExecuterInterface
+     */
+    private $executer;
+
+    /**
+     * The array of configured mailboxes
+     *
+     * @var array
+     */
+    private $mailboxes;
+
+
+    /**
+     * Initializes the Daemon with a Configuration and an Executer
+     *
+     * @param BradFeehan\Rainmaker\Configuration     $configuration
+     * @param BradFeehan\Rainmaker\ExecuterInterface $executer
+     */
+    public function __construct(Configuration $configuration, ExecuterInterface $executer = null)
+    {
+        $this->configuration = $configuration;
+        $this->executer = $executer;
+        $this->mailboxes = array();
+    }
+
+    /**
+     * Starts this Daemon
+     */
+    public function start()
+    {
+        $this->logger()->debug('Daemon::start()');
+        $this->getExecuter()->start();
+    }
+
+    /**
+     * Retrieves the Configuration for this Daemon
+     */
+    public function getConfiguration()
+    {
+        return $this->configuration;
+    }
+
+    /**
+     * Retrieves the Executer for this Daemon
+     *
+     * @return BradFeehan\Rainmaker\ExecuterInterface
+     */
+    public function getExecuter()
+    {
+        if ($this->executer === null) {
+            $this->executer = new Executer(
+                array($this, 'tick'),
+                $this->getConfiguration()->get('interval'),
+                $this->getConfiguration()->logger()
+            );
+        }
+
+        return $this->executer;
+    }
+
+    /**
+     * Retrieves the logger for this Daemon
+     *
+     * @return Psr\Log\LoggerInterface
+     */
+    public function logger()
+    {
+        return $this->getConfiguration()->logger();
+    }
+
+    /**
+     * The "tick" function for the Executer of this Daemon
+     *
+     * This is called periodically by this Daemon's Executer.
+     */
+    public function tick()
+    {
+        $this->logger()->debug('Daemon::tick()');
+
+        $this->logger()->info('Check starting');
+
+        // Iterate over all mailboxes
+        foreach ($this->getConfiguration()->getMailboxes() as $mailbox) {
+            $this->logger()->info(
+                "Checking mailbox '{$mailbox->getName()}'..."
+            );
+
+            $this->logger()->notice(
+                "Found {$mailbox->count()} feedback loop messages"
+            );
+
+            // Iterate over all feedback report messages in the mailbox
+            foreach ($mailbox as $feedbackLoopMessage) {
+                $this->processMessage($feedbackLoopMessage);
+            }
+        }
+
+        $this->logger()->info('Check successfully completed');
+    }
+
+    /**
+     * Processes an individual message
+     *
+     * @param BradFeehan\Rainmaker\FeedbackLoopMessage $message
+     */
+    public function processMessage($message)
+    {
+        // TODO
+    }
+}
---
+++ tests/BradFeehan/Rainmaker/Test/Unit/DaemonTest.php
@@ -0,0 +1,273 @@
+<?php
+
+namespace BradFeehan\Rainmaker\Test\Unit;
+
+use BradFeehan\Rainmaker\Daemon;
+use BradFeehan\Rainmaker\Test\UnitTestCase;
+use ReflectionClass;
+use ReflectionMethod;
+
+class DaemonTest extends UnitTestCase
+{
+
+    /**
+     * @covers BradFeehan\Rainmaker\Daemon::__construct
+     */
+    public function testConstruct()
+    {
+        $daemon = new Daemon(
+            \Mockery::mock('BradFeehan\\Rainmaker\\Configuration'),
+            \Mockery::mock('BradFeehan\\Rainmaker\\ExecuterInterface')
+        );
+
+        $this->assertInstanceOf(
+            'BradFeehan\\Rainmaker\\Daemon',
+            $daemon
+        );
+    }
+
+    /**
+     * @covers BradFeehan\Rainmaker\Daemon::start
+     */
+    public function testStart()
+    {
+        $logger = \Mockery::mock('Psr\\Log\\LoggerInterface')
+            ->shouldReceive('debug')
+                ->with('Daemon::start()')
+            ->getMock();
+
+        $executer = \Mockery::mock('BradFeehan\\Rainmaker\\ExecuterInterface')
+            ->shouldReceive('start')
+                ->withNoArgs()
+            ->getMock();
+
+        $daemon = $this->daemon('start')
+            ->shouldReceive('logger')
+                ->withNoArgs()
+                ->andReturn($logger)
+            ->shouldReceive('getExecuter')
+                ->withNoArgs()
+                ->andReturn($executer)
+            ->getMock();
+
+        $daemon->start();
+
+        // To prevent PHPUnit complaining about no assertions -- the
+        // real assertions are done by Mockery
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @covers BradFeehan\Rainmaker\Daemon::getConfiguration
+     */
+    public function testGetConfiguration()
+    {
+        $configuration = \Mockery::mock(
+            'BradFeehan\\Rainmaker\\Configuration'
+        );
+
+        $daemon = new Daemon($configuration);
+
+        $this->assertSame(
+            $configuration,
+            $daemon->getConfiguration()
+        );
+    }
+
+    /**
+     * @covers BradFeehan\Rainmaker\Daemon::getExecuter
+     */
+    public function testGetExecuter()
+    {
+        $configuration = \Mockery::mock(
+            'BradFeehan\\Rainmaker\\Configuration'
+        );
+
+        $executer = \Mockery::mock(
+            'BradFeehan\\Rainmaker\\ExecuterInterface'
+        );
+
+        $daemon = new Daemon($configuration, $executer);
+
+        $this->assertSame(
+            $executer,
+            $daemon->getExecuter()
+        );
+    }
+
+    /**
+     * @covers BradFeehan\Rainmaker\Daemon::getExecuter
+     */
+    public function testGetExecuterDefaultValue()
+    {
+        $logger = \Mockery::mock('Psr\\Log\\LoggerInterface');
+
+        $configuration = \Mockery::mock('BradFeehan\\Rainmaker\\Configuration')
+            ->shouldReceive('get')
+                ->with('interval')
+                ->andReturn(10)
+            ->shouldReceive('logger')
+                ->withNoArgs()
+                ->andReturn($logger)
+            ->getMock();
+
+        $daemon = $this->daemon('getExecuter')
+            ->shouldReceive('getConfiguration')
+                ->withNoArgs()
+                ->andReturn($configuration)
+            ->getMock();
+
+        $this->assertInstanceOf(
+            'BradFeehan\\Rainmaker\\ExecuterInterface',
+            $daemon->getExecuter()
+        );
+    }
+
+    /**
+     * @covers BradFeehan\Rainmaker\Daemon::logger
+     */
+    public function testLogger()
+    {
+        $configuration = \Mockery::mock('BradFeehan\\Rainmaker\\Configuration')
+            ->shouldReceive('logger')
+                ->withNoArgs()
+                ->andReturn('$logger')
+            ->getMock();
+
+        $daemon = $this->daemon('logger', 'configuration')
+            ->shouldReceive('getConfiguration')
+                ->withNoArgs()
+                ->andReturn($configuration)
+            ->getMock();
+
+        $this->assertSame(
+            '$logger',
+            $daemon->logger()
+        );
+    }
+
+    /**
+     * @covers BradFeehan\Rainmaker\Daemon::tick
+     */
+    public function testTick()
+    {
+        $logger = \Mockery::mock('Psr\\Log\\LoggerInterface')
+            ->shouldReceive('debug')
+                ->with('Daemon::tick()')
+                ->once()->ordered()
+            ->shouldReceive('info')
+                ->with('Check starting')
+                ->once()->ordered()
+            ->shouldReceive('info')
+                ->with('Checking mailbox \'$mailbox1\'...')
+                ->once()->ordered()
+            ->shouldReceive('notice')
+                ->with('Found $count1 feedback loop messages')
+                ->once()->ordered()
+            ->shouldReceive('info')
+                ->with('Checking mailbox \'$mailbox2\'...')
+                ->once()->ordered()
+            ->shouldReceive('notice')
+                ->with('Found $count2 feedback loop messages')
+                ->once()->ordered()
+            ->shouldReceive('info')
+                ->with('Check successfully completed')
+                ->once()->ordered()
+            ->getMock();
+
+        $message1 = \Mockery::mock();
+        $message2 = \Mockery::mock();
+
+        $mailbox1 = \Mockery::mock(
+            'ArrayIterator[getName,refresh,getInnerIterator,count]',
+            array(array($message1))
+        )
+            ->shouldReceive('getName')
+                ->withNoArgs()
+                ->andReturn('$mailbox1')
+            ->shouldReceive('count')
+                ->withNoArgs()
+                ->andReturn('$count1')
+            ->getMock();
+
+        $mailbox2 = \Mockery::mock(
+            'ArrayIterator[getName,refresh,getInnerIterator,count]',
+            array(array($message2))
+        )
+            ->shouldReceive('getName')
+                ->withNoArgs()
+                ->andReturn('$mailbox2')
+            ->shouldReceive('count')
+                ->withNoArgs()
+                ->andReturn('$count2')
+            ->getMock();
+
+        $mailboxes = array($mailbox1, $mailbox2);
+
+
+        $configuration = \Mockery::mock('BradFeehan\\Rainmaker\\Configuration')
+            ->shouldReceive('getMailboxes')
+                ->withNoArgs()
+                ->andReturn($mailboxes)
+            ->getMock();
+
+        $daemon = $this->daemon('tick')
+            ->shouldReceive('getConfiguration')
+                ->withNoArgs()
+                ->andReturn($configuration)
+            ->shouldReceive('logger')
+                ->withNoArgs()
+                ->andReturn($logger)
+            ->shouldReceive('processMessage')
+                ->with($message1)
+                ->once()
+            ->shouldReceive('processMessage')
+                ->with($message2)
+                ->once()
+            ->getMock();
+
+        $daemon->tick();
+
+        // To prevent PHPUnit complaining about no assertions -- the
+        // real assertions are done by Mockery
+        $this->assertTrue(true);
+    }
+
+
+    /**
+     * Creates a partially-mocked Daemon object
+     *
+     * Allows specifying which methods should NOT be mocked
+     *
+     * @param string $methodNameX A method that shouldn't be mocked
+     *
+     * @return BradFeehan\Rainmaker\Daemon
+     */
+    private function daemon(/* $methodName1, $methodName2, ... */)
+    {
+        $unmockedMethodNames = func_get_args();
+
+        // Determine which methods should be mocked
+        $mockedMethodNames = array();
+        $reflectionClass = new ReflectionClass('BradFeehan\\Rainmaker\\Daemon');
+
+        $allMethods = $reflectionClass->getMethods(ReflectionMethod::IS_PUBLIC);
+        foreach ($allMethods as $method) {
+            $methodName = $method->getName();
+
+            // Mock any methods that aren't in the unmocked method list
+            if (!in_array($methodName, $unmockedMethodNames)) {
+                $mockedMethodNames[] = $methodName;
+            }
+        }
+
+        // Create the class as a partial mock
+        $methods = implode(',', $mockedMethodNames);
+        return \Mockery::mock(
+            "BradFeehan\\Rainmaker\\Daemon[{$methods}]",
+            array(
+                \Mockery::mock('BradFeehan\\Rainmaker\\Configuration')
+            )
+        );
+    }
+}
